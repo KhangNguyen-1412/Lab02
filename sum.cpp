d222925ed7b48073a061d72ab363a84b346cda54
phuckhang <nhpk1412@gmail.com>
2024-03-18 16:49:57 +0700

    second cpp code

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath> 
+using namespace std; 
+
+int main() {
+    int a = 5; 
+    int b = 10; 
+    int sum = a + b; 
+    cout << "Tong hai so a va b la: " << sum << endl; 
+    
+    return 0;
+}
